a18e8f511881c26f588a1f472f5bbd75f36b4560
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-02-09 19:59:39 +0530

    Create UserGreeting.js

---
+++ UserGreeting.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+
+class UserGreeting extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    render(){
+        return(
+            this.state.isLoggedIn ?
+            <div>Ayush</div>:
+            <div>Guest</div>
+        )
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message=<div>Welcome my love!</div>
+        // }else{
+        //     message=<div>Welcome Guest...</div>
+        // }
+        // return <div>{message}</div>
+
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>Welcome Sonal!</div>
+        //     )
+        // }else{
+        //     return(
+        //         <div>Welcome Guest.</div>
+        //     )
+        // }
+        // return(
+        //     <div>
+        //         <div>Welcome Vishwas</div>
+        //         <div>Welcome Guest!</div>
+        //     </div>
+        // )
+    }
+}
+
+export default UserGreeting
