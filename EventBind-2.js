9548b20d331775e8291cbe1ea0a597deefb837c2
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-02-20 20:02:43 +0530

    Update EventBind-2.js

--- EventBind-2.js
+++ EventBind-2.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react'
 
-class EventBind extends Component {
+class EventBind extends Component{
 
     constructor(props){
         super(props)
 
         this.state = {
-            message: "Hello"
+            message: 'Hello'
         }
-        this.clickHandler = this.clickHandler.bind(this)
 
+        this.clickHandler = this.clickHandler.bind(this)
     }
 
-    clickHandler(){
+    // clickHandler(){
+    //     this.setState({
+    //         message: 'Goodbye!!'
+    //     })
+    //     console.log(this)
+    // }
+
+    clickHandler = () => {
         this.setState({
             message: 'Goodbye!'
         })
@@ -20,10 +27,12 @@ class EventBind extends Component {
 
     render(){
         return(
-            <div>
-                <div>{this.state.message}</div>
-                <button onClick={this.clickHandler}>Click!</button>
-            </div>
+         <div>
+             <div>{this.state.message}</div>
+             {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+             {/* <button onClick={() => this.clickHandler()}>Click</button> */}
+             <button onClick={this.clickHandler}>Click</button>
+         </div>   
         )
     }
 }
