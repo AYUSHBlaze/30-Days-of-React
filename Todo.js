03aae740fe142a69a13a646b5f35cf62259f553a
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-04-25 14:14:41 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -21,7 +21,7 @@ class TodoApp extends React.Component {
             value={this.state.text}
           />
           <button>
-            Add #{this.state.items.length + 1}
+           
           </button>
         </form>
       </div>
