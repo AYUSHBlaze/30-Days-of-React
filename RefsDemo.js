2eb7a9e7f66edd2de5f9ad9bc2ad83950c8fc7c2
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-04-26 17:05:59 +0530

    Add files via upload

--- RefsDemo.js
+++ RefsDemo.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
 class RefsDemo extends Component{
     constructor(props){
@@ -6,24 +6,24 @@ class RefsDemo extends Component{
         this.inputRef = React.createRef()
         this.cbRef = null
         this.setCbRef = (element) => {
-            this.cbRef = element
+            this.cbRef = element 
         }
     }
 
     componentDidMount(){
-        if (this.cbRef){
+        if(this.cbRef){
             this.cbRef.focus()
         }
-        // this.inputRef.current.focus
+        // this.inputRef.current.focus()
         // console.log(this.inputRef)
     }
-    
+
     clickHandler = () => {
         alert(this.inputRef.current.value)
     }
 
     render(){
-        return (
+        return(
             <div>
                 <input type="text" ref={this.inputRef}/>
                 <input type="text" ref={this.setCbRef}/>
