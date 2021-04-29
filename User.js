575d03f446769627d079ed75efc0870a59278ebb
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-04-29 20:07:23 +0530

    Render Props

---
+++ User.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class User extends Component{
+    render(){
+        return(
+            <div>
+                Ayush
+            </div>
+        )
+    }
+}
+
+export default User
