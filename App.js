214a275c7d79e00cfbf4336f7803003dc8af1582
Ayush Singh <60514130+AYUSHBlaze@users.noreply.github.com>
2021-06-01 21:33:10 +0530

    incomp

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
 import './App.css';
-import  Form from './components/Form'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-
-class App extends Component{
-  render(){
-
-    return(
+function App() {
+  return (
+    <Router>
       <div className="App">
-        <Form/>
+        <Nav/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/shop" component={Shop}/>
+        </Switch>
       </div>
-    )
-  }
+    </Router>
+  );
 }
+
+const Home = () => (
+  <div>
+    <h2>Home Page, Dawg</h2>
+  </div>
+);
 
 export default App;
